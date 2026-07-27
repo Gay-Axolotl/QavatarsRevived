b61921d2f72b0f8cd89d0d4e22c94e3bf5af025d
Gay-Axolotl <schmidaleksander58@gmail.com>
2026-07-27 21:13:35 +0200

    Add materialGenerator.cpp

---
+++ src/AssetLib/generators/materialGenerator.cpp
@@ -0,0 +1,62 @@
+#include "AssetLib/generators/materialGenerator.hpp"
+#include "AssetLib/modelImporter.hpp"
+
+namespace AssetLib::Generators
+{
+    template<>
+    UnityEngine::Material* MaterialGenerator::Generate(const VRMC_VRM_0_0::Vrm& vrm, int index, UnityEngine::Texture2D** textures)
+    {
+        auto [name, shader, renderQueue, floatProperties, vectorProperties, textureProperties, keywordMap, tagMap] = vrm.materialProperties[index];
+
+        auto mat = UnityEngine::Material::New_ctor(AssetLib::ModelImporter::mtoon.ptr());
+
+        for (const auto & [ key, value ] : floatProperties )
+        {
+            mat->SetFloat(key.c_str(), value);
+        }
+
+        for (const auto & [ key, value ] : keywordMap )
+        {
+            if(value)
+            {
+                mat->EnableKeyword(key.c_str());
+            }
+            else
+            {
+                mat->DisableKeyword(key.c_str());
+            }
+
+        }
+
+        for (const auto & [ key, value ] : textureProperties )
+        {
+            mat->SetTexture(key.c_str(), textures[value]);
+        }
+
+        for (const auto & [ key, value ] : vectorProperties )
+        {
+            mat->SetColor(key.c_str(), Sombrero::FastColor(value[0], value[1], value[2], value[3]));
+        }
+
+        for (const auto & [ key, value ] : tagMap )
+        {
+            mat->SetOverrideTag(key, value);
+        }
+
+        mat->set_renderQueue(static_cast<int32_t>(renderQueue));
+
+        return mat;
+    }
+
+    template<>
+    UnityEngine::Material* MaterialGenerator::Generate(const VRMC_VRM_1_0::Vrm& vrm, int index, UnityEngine::Texture2D** textures)
+    {
+        // NOTE: this stub is from the original VRM-Qavatars mod, not something
+        // cut by this port -- VRM 1.0 material generation was never implemented
+        // upstream. VRM1 avatars will load with null materials until this is
+        // written (would need mapping VRMC_materials_mtoon / PBR fallback onto
+        // the mToon shader or an equivalent).
+        //TODO: Implement VRMC_VRM_1_0::Vrm material generation
+        return nullptr;
+    }
+}
