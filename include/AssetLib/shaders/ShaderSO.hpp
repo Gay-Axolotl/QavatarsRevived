3225897695bc8a48c293975a8445b5633a8743b9
Gay-Axolotl <schmidaleksander58@gmail.com>
2026-07-27 21:14:00 +0200

    Add ShaderSO.hpp

---
+++ include/AssetLib/shaders/ShaderSO.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "QavatarsRevived/Main.hpp"
+#include "custom-types/shared/macros.hpp"
+#include "UnityEngine/ScriptableObject.hpp"
+#include "UnityEngine/Shader.hpp"
+#include "UnityEngine/Material.hpp"
+#include "UnityEngine/GameObject.hpp"
+
+DECLARE_CLASS_CODEGEN(VRMData, ShaderSO, UnityEngine::ScriptableObject,
+    DECLARE_INSTANCE_FIELD(UnityEngine::Shader*, mToonShader);
+    DECLARE_INSTANCE_FIELD(UnityEngine::Shader*, mirrorShader);
+    DECLARE_INSTANCE_FIELD(UnityEngine::Material*, shadowMaterial);
+)
