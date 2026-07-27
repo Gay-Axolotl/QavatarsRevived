2345fadde5a949e35247f5a1a59d235ccbaadf47
Gay-Axolotl <schmidaleksander58@gmail.com>
2026-07-27 07:46:16 +0200

    Add arrayUtils.hpp

---
+++ include/AssetLib/arrayUtils.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include "QavatarsRevived/Main.hpp"
+
+namespace ArrayUtils {
+    //borrowed from QuestUI
+    template <class Out, class T, class Predicate>
+    inline std::vector<Out> Select(std::vector<T> array, Predicate pred)
+    {
+        std::vector<Out> newArray(array.size()); 
+        for (int i = 0; i < array.size(); i++) { 
+            newArray[i] = pred(array[i]); 
+        } 
+        return newArray;
+    }
+}
