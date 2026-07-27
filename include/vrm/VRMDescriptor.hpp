33c85031fb6f3e6402d4f38a2fae395604e2329e
Gay-Axolotl <schmidaleksander58@gmail.com>
2026-07-27 08:39:30 +0200

    Add VRMDescriptor.hpp

---
+++ include/vrm/VRMDescriptor.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "vrm/vrmIncludes.hpp"
+#include "UnityEngine/Texture2D.hpp"
+namespace VRMQavatars
+{ 
+    struct VRMDescriptor
+    {
+        std::string filePath;
+        std::string name;
+        std::string author;
+        UnityEngine::Texture2D* thumbnail;
+        bool valid = true;
+
+        //TODO: Replace with union?
+        std::optional<VRMC_VRM_0_0::Vrm> vrm0;
+        std::optional<VRMC_VRM_1_0::Vrm> vrm1;
+    };
+
+    struct AvatarComparator
+    {
+        constexpr bool operator()(const VRMDescriptor& lhs, const VRMDescriptor& rhs) const
+        {
+            return lhs.name < rhs.name;
+        }
+    };
+};
