521d68c8383ec9c1eb66fd4e3b03cea9beb20dd4
Gay-Axolotl <schmidaleksander58@gmail.com>
2026-07-27 21:14:25 +0200

    Add shaderLoader.hpp

---
+++ include/AssetLib/shaders/shaderLoader.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "ShaderSO.hpp"
+#include "custom-types/shared/coroutine.hpp"
+
+#include "UnityEngine/AssetBundle.hpp"
+
+namespace AssetLib
+{
+    class ShaderLoader
+    {
+    public:
+        static SafePtrUnity<VRMData::ShaderSO> shaders;
+
+        static custom_types::Helpers::Coroutine LoadBund();
+
+        static custom_types::Helpers::Coroutine LoadBundleFromMemoryAsync(ArrayW<uint8_t> bytes, UnityEngine::AssetBundle*& out);
+
+        static custom_types::Helpers::Coroutine LoadAssetFromBundleAsync(UnityEngine::AssetBundle* bundle, std::string_view name, System::Type* type, UnityEngine::Object*& out);
+    };
+};
