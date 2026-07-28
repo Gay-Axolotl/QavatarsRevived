968406e4fc3d4f94732ff9e4703c9a2652958fe3
Gay-Axolotl <schmidaleksander58@gmail.com>
2026-07-28 07:11:27 +0200

    Wire up BSML::Init() and avatar picker registration

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include "QavatarsRevived/Main.hpp"
 #include "QavatarsRevived/AvatarLoader.hpp"
+#include "QavatarsRevived/UI/AvatarPicker.hpp"
+
+#include "bsml/shared/BSML.hpp"
 
 static ModInfo modInfo{MOD_ID, VERSION, 0};
 
@@ -8,10 +11,15 @@ extern "C" void setup(CModInfo* info) {
     VRMLogger.info("QavatarsRevived setup()");
 
     // TODO: install function hooks here once ported.
-    // Nothing scene/game-state dependent belongs in setup() -- that's late_load()'s job.
+    // Nothing scene/game-state dependent belongs in setup() -- that\'s late_load()\'s job.
 }
 
 extern "C" void late_load() {
     VRMLogger.info("QavatarsRevived late_load()");
+
+    // BSML::Init() must run before any BSML::Register::* call.
+    BSML::Init();
+
     QavatarsRevived::InitAvatarLoader();
+    QavatarsRevived::UI::RegisterAvatarPicker();
 }
