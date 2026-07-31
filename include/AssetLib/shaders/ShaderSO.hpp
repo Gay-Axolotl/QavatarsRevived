abc1b95c755298e9cee51953bf6ab64f233bd3dc
Gay-Axolotl <schmidaleksander58@gmail.com>
2026-07-31 23:27:25 +0200

    Fix: DECLARE_CLASS_CODEGEN in this custom-types version has no variadic body param -- use DECLARE_CLASS_CODEGEN_INTERFACES instead (confirmed via CI header inspection)

--- include/AssetLib/shaders/ShaderSO.hpp
+++ include/AssetLib/shaders/ShaderSO.hpp
@@ -7,7 +7,13 @@
 #include "UnityEngine/Material.hpp"
 #include "UnityEngine/GameObject.hpp"
 
-DECLARE_CLASS_CODEGEN(VRMData, ShaderSO, UnityEngine::ScriptableObject,
+// NOTE: DECLARE_CLASS_CODEGEN in this custom-types version (0.18.3) only
+// takes 3 fixed args (namespace, name, baseT) -- no inline body/variadic
+// field-declaration block, unlike the pattern shown in the BSMG wiki and
+// porting guide (which describe an older custom-types API). Using
+// DECLARE_CLASS_CODEGEN_INTERFACES instead, which does support a trailing
+// variadic block, passing zero interfaces.
+DECLARE_CLASS_CODEGEN_INTERFACES(VRMData, ShaderSO, UnityEngine::ScriptableObject,
     DECLARE_INSTANCE_FIELD(UnityEngine::Shader*, mToonShader);
     DECLARE_INSTANCE_FIELD(UnityEngine::Shader*, mirrorShader);
     DECLARE_INSTANCE_FIELD(UnityEngine::Material*, shadowMaterial);
