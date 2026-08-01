6688e64ccde902ff5c5389aae196766d4e27dcb4
Gay-Axolotl <schmidaleksander58@gmail.com>
2026-08-01 15:03:12 +0200

    Implement runtime field getters for the bundled ShaderSO asset

--- src/AssetLib/shaders/ShaderSO.cpp
+++ src/AssetLib/shaders/ShaderSO.cpp
@@ -1,3 +1,20 @@
 #include "AssetLib/shaders/ShaderSO.hpp"
 
-DEFINE_TYPE(VRMData, ShaderSO);
+#include "beatsaber-hook/shared/utils/il2cpp-utils.hpp"
+
+namespace VRMData::ShaderSOFields {
+    UnityEngine::Shader* GetMToonShader(UnityEngine::ScriptableObject* instance) {
+        auto result = il2cpp_utils::GetFieldValue<UnityEngine::Shader*>(instance, "mToonShader");
+        return result.has_value() ? result.value() : nullptr;
+    }
+
+    UnityEngine::Shader* GetMirrorShader(UnityEngine::ScriptableObject* instance) {
+        auto result = il2cpp_utils::GetFieldValue<UnityEngine::Shader*>(instance, "mirrorShader");
+        return result.has_value() ? result.value() : nullptr;
+    }
+
+    UnityEngine::Material* GetShadowMaterial(UnityEngine::ScriptableObject* instance) {
+        auto result = il2cpp_utils::GetFieldValue<UnityEngine::Material*>(instance, "shadowMaterial");
+        return result.has_value() ? result.value() : nullptr;
+    }
+}
